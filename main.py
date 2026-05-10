951401a3767844a2705d95caf7553ce492ae1dfb
KaalVenom <shubhamk_ug_22@ece.nits.ac.in>
2026-05-11 01:26:24 +0530

    Fixed Gemini model

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
-gemini_model = genai.GenerativeModel("models/gemini-2.5-flash")
+gemini_model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 
